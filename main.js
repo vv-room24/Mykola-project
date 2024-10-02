3260d8d94ad7f7bfc984441aa69b0fbb2f90023c
FIRST_NAME LAST_NAME <soundsoldier61@gmail.com>
2024-10-02 09:49:22 +0300

    added menu buttons, main section, css and js handl

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log(2);
+function showContent(contentId) {
+    const contentBlocks = document.querySelectorAll('.content div[data-content]');
+    contentBlocks.forEach(block => {
+        block.classList.remove('active');
+    });
+    const activeBlock = document.getElementById(contentId + 'Section');
+    activeBlock.classList.add('active');
+}
