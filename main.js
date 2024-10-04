3ced7c32410936a172eddfdfb69dd929f36f0d0c
Mykola Usatyi <us.mykola@gmail.com>
2024-10-04 17:37:07 +0300

    added menu showContent

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log(2);
+function showContent(contentId) {
+    // Отримати всі блоки з контентом
+    const contentBlocks = document.querySelectorAll('.content div');
+    
+    // Приховати всі блоки
+    contentBlocks.forEach(block => {
+        block.classList.remove('active');
+    });
+    
+    // Показати потрібний блок
+    const activeBlock = document.getElementById(contentId);
+    activeBlock.classList.add('active');
+    }
+    
